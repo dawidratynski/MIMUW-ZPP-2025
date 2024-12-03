5a9568f0574f88e2dafd68fec93312f564ad93e3
Dawid Ratyński <dawidratynski@gmail.com>
2024-12-03 16:30:11 +0100

    Add basic item models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,24 @@
 from sqlmodel import Field, SQLModel
+from data_enums import TrashType
 
-class Item(SQLModel, table=True):
+
+# Common public data about Items
+class ItemBase(SQLModel):
+    type: TrashType = Field(default=TrashType.other)
+    latitude: float = Field()
+    longitude: float = Field()
+    
+
+# Data about Items stored in db
+class Item(ItemBase, table=True):
     id: int = Field(default=None, primary_key=True)
-    image_id: int = Field(default=None)
+
+
+# Data returned about an item
+class ItemPublic(ItemBase):
+    id: int
+
+
+# Data needed to add a new Item
+class ItemCreate(ItemBase):
+    pass
