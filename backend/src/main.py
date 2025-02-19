0ca01c03bc898d2c15a26b55e2316f1e341bbcc1
Dawid Ratyński <dawidratynski@gmail.com>
2025-02-19 12:03:51 +0100

    Basic backend integration with Auth0

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Security
 from fastapi.middleware.cors import CORSMiddleware
 
+from utils.auth import VerifyToken
+
 app = FastAPI()
+auth = VerifyToken()
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,3 +17,28 @@ app.add_middleware(
 @app.get("/")
 def home():
     return "Hello, World!"
+
+@app.get("/api/public")
+def public():
+    """No access token required to access this route"""
+
+    result = {
+        "status": "success",
+        "msg": ("Hello from a public endpoint! You don't need to be "
+                "authenticated to see this.")
+    }
+    return result
+
+@app.get("/api/private")
+def private(auth_result: str = Security(auth.verify)):
+    """A valid access token is required to access this route"""
+    return auth_result
+
+
+@app.get("/api/private-scoped")
+def private_scoped(auth_result: str = Security(auth.verify, scopes=['dev:test_scope'])):
+    """A valid access token and an appropriate scope are required to access
+    this route
+    """
+
+    return auth_result
