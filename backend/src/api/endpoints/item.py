38c84e55956dc79b424830c76da31be5751cc5f7
Dawid Ratyński <dawidratynski@gmail.com>
2025-03-01 16:29:10 +0100

    Fix Item models and endpoints

--- backend/src/api/endpoints/item.py
+++ backend/src/api/endpoints/item.py
@@ -1,51 +1,105 @@
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, List
 from uuid import uuid4
 
-from fastapi import APIRouter, HTTPException, Query, Security
+import aiofiles
+from fastapi import APIRouter, Form, HTTPException, Query, Security
+from pydantic import TypeAdapter
+from pydantic_core import ValidationError
 from sqlmodel import select
 
 from core.auth import VerifyToken
 from core.db import SessionDep
-from core.models.item import Item, ItemCreate, ItemResponse
+from core.models.item import (
+    BoundingBox,
+    BoundingBoxRequest,
+    Item,
+    ItemCreate,
+    ItemResponse,
+)
 
 auth = VerifyToken()
 
 router = APIRouter(prefix="/item")
 
 
-# TODO: Add photo upload
+async def _validate_item_submission(
+    item: ItemCreate, bounding_boxes: list[BoundingBoxRequest]
+):
+    """Checks if the item and photo are a valid submission"""
+    # TODO: Check if file is a photo (maybe using PIL?)
+    # TODO: Check if file name is valid
+    # TODO: Check if file extension is acceptable and matches detected by PIL
+    # TODO: Check if file is acceptable size
+    # TODO: Check if all bounding box coords are within the photo
+    pass
+
+
+async def _validate_and_save_submission(
+    item: ItemCreate, bounding_boxes: list[BoundingBoxRequest]
+) -> str:
+    """Validates the photo and item. If needed, resizes the photo and
+    bounding boxes. Returns photo id."""
+
+    await _validate_item_submission(item, bounding_boxes)
+
+    photo_id = uuid4().hex
+    photo_ext = item.photo.filename.split(".")[-1]
+    photo_filename = photo_id + "." + photo_ext
+
+    # Use async file io to not block main event loop
+    async with aiofiles.open(f"/{photo_filename}", "wb+") as f:
+        while photo_chunk := await item.photo.read(1024):
+            await f.write(photo_chunk)
+
+    return photo_id
+
+
 @router.post("/submit", response_model=ItemResponse)
 async def create_item(
-    item: ItemCreate, session: SessionDep, auth_result: str = Security(auth.verify)
+    item: Annotated[ItemCreate, Form(media_type="multipart/form-data")],
+    session: SessionDep,
+    auth_result: str = Security(auth.verify),
 ):
+    try:
+        bounding_boxes = [
+            BoundingBox.from_request(bb)
+            for bb in TypeAdapter(List[BoundingBoxRequest]).validate_json(
+                item.bounding_boxes_json
+            )
+        ]
+    except ValidationError:
+        raise HTTPException(status_code=400, detail="Provided bounding boxes JSON is invalid")
+
+    photo_id = await _validate_and_save_submission(item, bounding_boxes)
+
     item = Item(
-        id=None,
-        photo_id=uuid4().hex,
-        uploaded_at=datetime.now(),
-        bounding_boxes=[],  # TODO - Insert bounding boxes to db
         **item.model_dump(),
+        photo_id=photo_id,
+        uploaded_at=datetime.now(),
+        bounding_boxes=bounding_boxes,
     )
-    db_item = Item.model_validate(item)
-    session.add(db_item)
+
+    saved_item = Item.model_validate(item)
+    session.add(saved_item)
     session.commit()
-    session.refresh(db_item)
-    return db_item.into_response_model()
+    session.refresh(saved_item)
+    return saved_item.into_response()
 
 
 @router.get("/list", response_model=list[ItemResponse])
-async def list_items(
+def list_items(
     session: SessionDep,
     offset: Annotated[int, Query(ge=0)] = 0,
     limit: Annotated[int, Query(le=50)] = 50,
 ):
     items = session.exec(select(Item).offset(offset).limit(limit)).all()
-    return [item.into_response_model() for item in items]
+    return [item.into_response() for item in items]
 
 
 # TODO - Complete rework
 @router.get("/search", response_model=list[ItemResponse])
-async def search_items(
+def search_items(
     session: SessionDep,
     offset: Annotated[int, Query(ge=0)] = 0,
     limit: Annotated[int, Query(le=50)] = 50,
@@ -65,14 +119,14 @@ async def search_items(
         .limit(limit)
     ).all()
 
-    return [item.into_response_model() for item in items]
+    return [item.into_response() for item in items]
 
 
 @router.get("/{item_id}", response_model=ItemResponse)
-async def get_item(item_id: int, session: SessionDep):
+def get_item(item_id: int, session: SessionDep):
     item = session.get(Item, item_id)
 
     if not item:
         raise HTTPException(status_code=404, detail="No item with given id found")
 
-    return item.into_response_model()
+    return item.into_response()
--- backend/src/core/models/item.py
+++ backend/src/core/models/item.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from enum import Enum
-from typing import Optional
+from typing import Self
 
-from pydantic import BaseModel
+from fastapi import UploadFile
+from pydantic import model_validator
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -14,70 +15,82 @@ class ItemType(str, Enum):
     unknown = "unknown"
 
 
-class PhotoPixel(BaseModel):
-    x: int = Field(ge=0)
-    y: int = Field(ge=0)
+class BoundingBoxBase(SQLModel):
+    item_type: ItemType
+    x_left: int = Field(ge=0)
+    x_right: int = Field(ge=0)
+    y_top: int = Field(ge=0)
+    y_bottom: int = Field(ge=0)
+
+    @model_validator(mode="after")
+    def check_coordinates(self) -> Self:
+        if not self.x_left < self.x_right:
+            raise ValueError("x_left must be less than x_right")
+
+        if not self.y_top < self.y_bottom:
+            raise ValueError("y_top must be less than y_bottom")
+
+        return self
 
 
-class BoundingBox(SQLModel, table=True):  # type: ignore
-    id: Optional[int] = Field(default=None, primary_key=True)
+class BoundingBoxRequest(BoundingBoxBase):
+    pass
+
+
+class BoundingBoxResponse(BoundingBoxBase):
+    pass
+
+
+class BoundingBox(BoundingBoxBase, table=True):  # type: ignore
+    id: int | None = Field(default=None, primary_key=True)
 
     item_id: int = Field(foreign_key="item.id")
     item: "Item" = Relationship(back_populates="bounding_boxes")
 
-    type: ItemType
+    def into_response(self) -> BoundingBoxResponse:
+        return BoundingBoxResponse(**self.model_dump())
 
-    left_upper_corner_x: int = Field(ge=0)
-    left_upper_corner_y: int = Field(ge=0)
-    right_bottom_corner_x: int = Field(ge=0)
-    right_bottom_corner_y: int = Field(ge=0)
-
-    def into_response_model(self) -> tuple[PhotoPixel, PhotoPixel, ItemType]:
-        return (
-            PhotoPixel(x=self.left_upper_corner_x, y=self.left_upper_corner_y),
-            PhotoPixel(x=self.right_bottom_corner_x, y=self.right_bottom_corner_y),
-            self.type,
+    @staticmethod
+    def from_request(
+        bb: BoundingBoxRequest, item_id: str | None = None
+    ) -> "BoundingBox":
+        """Intended for use only when creating item"""
+        return BoundingBox(
+            **bb.model_dump(),
+            item_id=item_id,
         )
 
 
-class ItemCreate(BaseModel):
+class ItemBase(SQLModel):
     user_id: str
     created_at: datetime
     latitude: float = Field(ge=-90, le=90)
     longitude: float = Field(ge=-180, le=180)
-    boundng_boxes: list[tuple[PhotoPixel, PhotoPixel, ItemType]]
 
 
-class ItemResponse(BaseModel):
+class ItemCreate(ItemBase):
+    photo: UploadFile
+    bounding_boxes_json: (
+        str  # JSON-encoded list[BoundingBoxRequest], str due to a bug in swaggerui
+    )
+
+
+class ItemResponse(ItemBase):
     id: int = Field(default=None, primary_key=True)
     photo_id: str
     user_id: str
-
-    created_at: datetime
     uploaded_at: datetime
-    latitude: float = Field(ge=-90, le=90)
-    longitude: float = Field(ge=-180, le=180)
-
-    bounding_boxes: list[tuple[PhotoPixel, PhotoPixel, ItemType]]
+    bounding_boxes: list[BoundingBoxResponse]
 
 
-class Item(SQLModel, table=True):  # type: ignore
-    id: Optional[int] = Field(default=None, primary_key=True)
+class Item(ItemBase, table=True):  # type: ignore
+    id: int | None = Field(default=None, primary_key=True)
     photo_id: str
-    user_id: str
-
-    created_at: datetime
     uploaded_at: datetime
-    latitude: float = Field(ge=-90, le=90)
-    longitude: float = Field(ge=-180, le=180)
-
     bounding_boxes: list[BoundingBox] = Relationship(back_populates="item")
 
-    def into_response_model(self) -> ItemResponse:
-        data = self.model_dump()
-        data["bounding_boxes"] = [
-            bb.into_response_model() for bb in self.bounding_boxes
-        ]
+    def into_response(self) -> ItemResponse:
         return ItemResponse(
-            **data,
+            **self.model_dump(),
+            bounding_boxes=[bb.into_response() for bb in self.bounding_boxes],
         )
