e7266b64def66daf78e195fdd94ccf68bacf79e2
Dawid Ratyński <dawidratynski@gmail.com>
2025-04-07 17:16:41 +0200

    Add map frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
