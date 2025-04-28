6bbf112848fdc54fe8cf70d22242dcd6d0533eb0
Dawid Ratyński <dawidratynski@gmail.com>
2025-04-28 13:09:43 +0200

    Add basic auth to frontend app

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const auth0 = useAuth0();
+
+const user = auth0.user;
+const isAuthenticated = auth0.isAuthenticated;
+const isLoading = auth0.isLoading;
+
+
+async function login() {
+    await auth0.loginWithRedirect();
+    await auth0.getAccessTokenSilently();
+}
+
+async function logout() {
+    await auth0.logout({
+        logoutParams: {
+            returnTo: window.location.origin
+        }
+    });
+}
+
+</script>
+
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 10px;">
         <div class="navbar-brand" href="#">CleanTheWorld</div>
@@ -19,10 +44,12 @@
                 </li>
             </ul>
             <ul class="navbar-nav ms-auto">
+                <li class="nav-item" v-if="isAuthenticated">
+                    <div class="navbar-text"> Logged in as: {{ user.email }} </div>
+                </li>
                 <li class="nav-item">
-                    <RouterLink class="nav-link" to="/login">
-                        Login
-                    </RouterLink>
+                    <button v-if="isAuthenticated" @click="logout" class="nav-link">Log out</button>
+                    <button v-if="!isAuthenticated && !isLoading" @click="login" class="nav-link">Log in</button>
                 </li>
             </ul>
         </div>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,18 @@ import 'bootstrap'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import { createAuth0 } from '@auth0/auth0-vue'
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from '@/env'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(
+    createAuth0({
+      domain: AUTH0_DOMAIN,
+      clientId: AUTH0_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  )
+  .use(router)
+  .mount('#app')
