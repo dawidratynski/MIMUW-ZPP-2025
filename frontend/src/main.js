a5e49b0145d4b1024da9d410e01666b15e2d1365
w-aleksandra <73110718+w-aleksandra@users.noreply.github.com>
2025-05-05 01:35:19 +0200

    Handle internationalization

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,26 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import router from './router'
 import App from './App.vue'
 import { createAuth0 } from '@auth0/auth0-vue'
 import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from '@/env'
+
+import en from './locales/en.json'
+import pl from './locales/pl.json'
+
+const savedLocale = localStorage.getItem('app-locale') || 'pl'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: savedLocale,
+  fallbackLocale: 'pl',
+  messages: {
+    en,
+    pl
+  }
+})
 
 createApp(App)
   .use(
@@ -18,4 +34,5 @@ createApp(App)
     }),
   )
   .use(router)
+  .use(i18n)
   .mount('#app')
